9994ad615891ab954bddb3e0013c88a25adb8f19
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-05 19:34:44 +0200

    added GlobalStyle to project

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,9 @@
 import { Normalize } from 'styled-normalize';
+import { GlobalStyle } from './GlobalStyle';
 
 const App = () => (
   <>
+    <GlobalStyle />
     <Normalize />
     <div>Hello world!</div>
   </>

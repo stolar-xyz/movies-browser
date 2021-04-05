d3b61bcb61c5c2064ea41858ebe30edfc5039195
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-05 19:40:41 +0200

    added ThemeProvider and empty theme.js file

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import { Normalize } from 'styled-normalize';
 import { GlobalStyle } from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
 
 const App = () => (
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyle />
     <Normalize />
     <div>Hello world!</div>
-  </>
+  </ThemeProvider>
 );
 
 export default App;

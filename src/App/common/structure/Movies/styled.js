5f753f8f2637c93d9c9371dfdd9bd44f91070ef7
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-10 21:21:58 +0200

    decreased columns size

--- src/App/common/structure/Movies/styled.js
+++ src/App/common/structure/Movies/styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledMovies = styled.ul`
   display: grid;
-  grid-template-columns: repeat(auto-fill, 324px);
+  grid-template-columns: repeat(auto-fill, 314px);
   grid-gap: 24px;
   justify-content: center;
   margin: 24px 0 40px;

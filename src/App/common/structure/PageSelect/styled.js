c066119caff0490fe1193a257391d0fe43865352
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-13 21:40:31 +0200

    added media queries styles to max width 767px

--- src/App/common/structure/PageSelect/styled.js
+++ src/App/common/structure/PageSelect/styled.js
@@ -3,10 +3,13 @@ import { ReactComponent as Arrowhead } from '../../../images/svgs/arrowhead.svg'
 
 export const StyledPageSelect = styled.div`
   margin: 0 auto 103px;
-  max-width: 525px;
+  width: max-content;
   display: flex;
-  justify-content: space-between;
   align-items: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    margin: 0 auto 31px;
+  }
 `;
 
 export const ArrowheadIcon = styled(Arrowhead)`
@@ -19,6 +22,12 @@ export const ArrowheadIcon = styled(Arrowhead)`
       transform: rotate(180deg);
       margin: 0 0 0 8px;
     `}
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    margin: 0;
+    width: 5px;
+    height: 8px;
+  }
 `;
 
 export const PageButton = styled.button`
@@ -30,6 +39,7 @@ export const PageButton = styled.button`
   cursor: pointer;
   color: #000000;
   padding: 8px 16px;
+  margin: 0 6px;
   background: #d6e4ff;
   border-radius: 5px;
   transition: box-shadow 0.3s;
@@ -50,13 +60,30 @@ export const PageButton = styled.button`
       color: #7e839a;
     }
   }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    margin: 0 4px;
+    padding: 8px 12px;
+  }
+`;
+
+export const PageButtonText = styled.span`
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    display: none;
+  }
 `;
 
 export const Wrapper = styled.div`
   display: inline-grid;
   grid-auto-flow: column;
-  padding: 0 12px;
+  margin: 0 18px;
   grid-gap: 8px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    grid-gap: 4px;
+    font-size: 10px;
+    margin: 0 4px;
+  }
 `;
 
 export const PageText = styled.span`

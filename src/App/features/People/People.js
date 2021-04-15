12e5f90c55bef9099a02228bcda02727ae197059
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-15 21:10:29 +0200

    created test People structure

--- src/App/features/People/People.js
+++ src/App/features/People/People.js
@@ -1,3 +1,41 @@
-const People = () => <div>hello world!</div>;
+import Subheader from '../../common/styled/Subheader';
+import Movie from '../Movies/Movie';
+import { StyledMovies } from '../Movies/styled';
+
+const People = () => (
+  <>
+    <Subheader title={'People'} />
+    <StyledMovies>
+      <Movie
+        title={'Mulan'}
+        year={'2020'}
+        genres={'Action'}
+        rate={'7,8'}
+        votes={'35'}
+      />
+      <Movie
+        title={'Mulan'}
+        year={'2020'}
+        genres={'Action'}
+        rate={'7,8'}
+        votes={'35'}
+      />
+      <Movie
+        title={'Mulan'}
+        year={'2020'}
+        genres={'Action'}
+        rate={'7,8'}
+        votes={'35'}
+      />
+      <Movie
+        title={'Mulan'}
+        year={'2020'}
+        genres={'Action'}
+        rate={'7,8'}
+        votes={'35'}
+      />
+    </StyledMovies>
+  </>
+);
 
 export default People;

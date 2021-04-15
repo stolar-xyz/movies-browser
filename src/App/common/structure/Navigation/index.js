4f0f5856812d15685aabbb4d99b1e373250772f2
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-15 19:53:22 +0200

    added paths to Navigation and header

--- src/App/common/structure/Navigation/index.js
+++ src/App/common/structure/Navigation/index.js
@@ -10,17 +10,27 @@ import {
   Title,
   Wrapper,
 } from './styled';
+import { NavLink } from 'react-router-dom';
+import { toMovies, toPeople } from '../../../routes';
 
 const Navigation = () => (
   <StyledNavigation>
     <Wrapper>
       <Header>
-        <VideoIcon />
-        <Title>Movies&nbsp;browser</Title>
+        <Title>
+          <NavLink to={toMovies()}>
+            <VideoIcon />
+            Movies&nbsp;browser
+          </NavLink>
+        </Title>
       </Header>
       <List>
-        <ListItem>Movies</ListItem>
-        <ListItem>Pepole</ListItem>
+        <ListItem>
+          <NavLink to={toMovies()}>Movies</NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink to={toPeople()}>People</NavLink>
+        </ListItem>
       </List>
       <Search>
         <SearchIcon />

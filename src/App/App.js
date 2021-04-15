a5186791055280ff69397349b898332902fdfc4e
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-15 19:56:25 +0200

    added HashRouter, Switch, Route and Redirect from react-router-dom with paths

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,35 @@
 import Navigation from './common/structure/Navigation';
 import Main from './common/styled/Main';
-import Subheader from './common/styled/Subheader';
 import Movies from './features/Movies/Movies';
+import People from './features/People/People';
 import PageSelect from './common/structure/PageSelect';
 import { Normalize } from 'styled-normalize';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { toMovies, toPeople } from './routes';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
     <Normalize />
-    <Navigation />
-    <Main>
-      <Subheader title={'Popular movies'} />
-      <Movies />
-    </Main>
+    <HashRouter>
+      <Navigation />
+      <Main>
+        <Switch>
+          <Route path={toMovies()}>
+            <Movies />
+          </Route>
+          <Route path={toPeople()}>
+            <People />
+          </Route>
+          <Route>
+            <Redirect to={toMovies()} />
+          </Route>
+        </Switch>
+      </Main>
+    </HashRouter>
     <PageSelect minNumber={'1'} maxNumber={'500'} />
   </ThemeProvider>
 );

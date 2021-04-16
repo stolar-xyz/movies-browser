5314a25f8e422193a8e66e2037adc32f2e195a6e
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-16 18:36:18 +0200

    added StyledPeople component

---
+++ src/App/features/People/styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledPeople = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 180px);
+  grid-gap: 24px;
+  justify-content: center;
+  margin: 24px 0 0;
+  padding: 0;
+  list-style: none;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    grid-gap: 16px;
+    grid-template-columns: repeat(auto-fill, 120px);
+    margin: 12px 0 0;
+  }
+`;

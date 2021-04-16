7e0ffb4594033a74385e1cfd32819e2822176e6b
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-16 19:52:54 +0200

    increased column size

--- src/App/features/People/styled.js
+++ src/App/features/People/styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledPeople = styled.ul`
   display: grid;
-  grid-template-columns: repeat(auto-fill, 180px);
+  grid-template-columns: repeat(auto-fill, 185px);
   grid-gap: 24px;
   justify-content: center;
   margin: 24px 0 0;
@@ -11,7 +11,7 @@ export const StyledPeople = styled.ul`
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     grid-gap: 16px;
-    grid-template-columns: repeat(auto-fill, 120px);
+    grid-template-columns: repeat(auto-fill, 136px);
     margin: 12px 0 0;
   }
 `;

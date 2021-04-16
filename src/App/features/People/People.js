12435239d46bef1240c190f967437584b66c51da
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-16 18:41:11 +0200

    deleted test structure and added right components with example props values

--- src/App/features/People/People.js
+++ src/App/features/People/People.js
@@ -1,40 +1,21 @@
 import Subheader from '../../common/styled/Subheader';
-import Movie from '../movies/Movie';
-import { StyledMovies } from '../movies/styled';
+import Person from './Person';
+import { StyledPeople } from './styled';
 
 const People = () => (
   <>
-    <Subheader title={'People'} />
-    <StyledMovies>
-      <Movie
-        title={'Mulan'}
-        year={'2020'}
-        genres={'Action'}
-        rate={'7,8'}
-        votes={'35'}
-      />
-      <Movie
-        title={'Mulan'}
-        year={'2020'}
-        genres={'Action'}
-        rate={'7,8'}
-        votes={'35'}
-      />
-      <Movie
-        title={'Mulan'}
-        year={'2020'}
-        genres={'Action'}
-        rate={'7,8'}
-        votes={'35'}
-      />
-      <Movie
-        title={'Mulan'}
-        year={'2020'}
-        genres={'Action'}
-        rate={'7,8'}
-        votes={'35'}
-      />
-    </StyledMovies>
+    <Subheader title={'Popular people'} />
+    <StyledPeople>
+      <Person name={'Henry Cavill'} />
+      <Person name={'Henry Cavill'} />
+      <Person name={'Henry Cavill'} />
+      <Person name={'Henry Cavill'} />
+      <Person name={'Henry Cavill'} />
+      <Person name={'Henry Cavill'} />
+      <Person name={'Henry Cavill'} />
+      <Person name={'Henry Cavill'} />
+      <Person name={'Henry Cavill'} />
+    </StyledPeople>
   </>
 );
 

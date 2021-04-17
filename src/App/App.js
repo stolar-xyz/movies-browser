3087fe90533c2fe4b7d40d1b3ca093c7aaae4444
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-17 16:53:18 +0200

    added MoviePage to App.js structure for testing

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import Navigation from './common/structure/Navigation';
 import Main from './common/styled/Main';
+import Section from './common/styled/Section';
 import Movies from './features/movies/Movies';
 import People from './features/people/People';
 import PageSelect from './common/structure/PageSelect';
-import Section from './common/styled/Section';
+import MoviePage from './features/moviePage/MoviePage';
 import { Normalize } from 'styled-normalize';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
@@ -20,6 +21,7 @@ const App = () => (
       <Main>
         <Switch>
           <Route path={toMovies()}>
+            <MoviePage title={'Mulan'} rate={'7,8'} votes={'335'} />
             <Section>
               <Movies />
             </Section>

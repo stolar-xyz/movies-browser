5b7196c80d677cd306dd4b258273776f5b83c741
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-17 15:37:16 +0200

    changed Main component styles

--- src/App/common/styled/Main/index.js
+++ src/App/common/styled/Main/index.js
@@ -2,12 +2,6 @@ import styled from 'styled-components';
 
 const Main = styled.main`
   max-width: 1400px;
-  margin: 56px auto 0;
-  padding: 0 16px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-    margin: 24px auto 32px;
-  }
 `;
 
 export default Main;

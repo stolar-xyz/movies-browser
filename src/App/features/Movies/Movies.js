d760461d2a641648ff132f4a04db7151a6d8b3cb
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-19 12:45:51 +0200

    added Container styled component to structure

--- src/App/features/Movies/Movies.js
+++ src/App/features/Movies/Movies.js
@@ -1,43 +1,42 @@
 import Subheader from '../../common/styled/Subheader';
 import Movie from '../../common/structure/Movie';
+import Container from '../../common/styled/Container';
 import { StyledMovies } from './styled';
 
-const Movies = () => {
-  return (
-    <>
-      <Subheader>Popular movies</Subheader>
-      <StyledMovies>
-        <Movie
-          title={'Mulan'}
-          year={'2020'}
-          genres={'Action'}
-          rate={'7,8'}
-          votes={'35'}
-        />
-        <Movie
-          title={'Mulan'}
-          year={'2020'}
-          genres={'Action'}
-          rate={'7,8'}
-          votes={'35'}
-        />
-        <Movie
-          title={'Mulan'}
-          year={'2020'}
-          genres={'Action'}
-          rate={'7,8'}
-          votes={'35'}
-        />
-        <Movie
-          title={'Mulan'}
-          year={'2020'}
-          genres={'Action'}
-          rate={'7,8'}
-          votes={'35'}
-        />
-      </StyledMovies>
-    </>
-  );
-};
+const Movies = () => (
+  <Container>
+    <Subheader>Popular movies</Subheader>
+    <StyledMovies>
+      <Movie
+        title={'Mulan'}
+        year={'2020'}
+        genres={'Action'}
+        rate={'7,8'}
+        votes={'35'}
+      />
+      <Movie
+        title={'Mulan'}
+        year={'2020'}
+        genres={'Action'}
+        rate={'7,8'}
+        votes={'35'}
+      />
+      <Movie
+        title={'Mulan'}
+        year={'2020'}
+        genres={'Action'}
+        rate={'7,8'}
+        votes={'35'}
+      />
+      <Movie
+        title={'Mulan'}
+        year={'2020'}
+        genres={'Action'}
+        rate={'7,8'}
+        votes={'35'}
+      />
+    </StyledMovies>
+  </Container>
+);
 
 export default Movies;
--- src/App/features/People/People.js
+++ src/App/features/People/People.js
@@ -1,9 +1,10 @@
 import Subheader from '../../common/styled/Subheader';
 import Person from '../../common/structure/Person';
+import Container from '../../common/styled/Container';
 import { StyledPeople } from './styled';
 
 const People = () => (
-  <>
+  <Container>
     <Subheader>Popular people</Subheader>
     <StyledPeople>
       <Person name={'Henry Cavill'} />
@@ -16,7 +17,7 @@ const People = () => (
       <Person name={'Henry Cavill'} />
       <Person name={'Henry Cavill'} />
     </StyledPeople>
-  </>
+  </Container>
 );
 
 export default People;

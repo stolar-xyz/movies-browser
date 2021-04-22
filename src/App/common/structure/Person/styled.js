e7322d99636cab98d6acd27b9f3c7cf4b8955bd2
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-22 19:10:15 +0200

    added Role styled component and changed styles

--- src/App/common/structure/Person/styled.js
+++ src/App/common/structure/Person/styled.js
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-grow: 1;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  display: grid;
+  grid-gap: 8px;
   padding: 16px;
-  transition: filter 0.3s, box-shadow 0.3s ease-out;
+  flex-grow: 1;
+  text-align: center;
+  transition: box-shadow 0.3s ease-out;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     padding: 8px;
@@ -19,36 +18,42 @@ export const StyledPerson = styled.li`
   background: #ffffff;
   flex-direction: column;
   box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-  height: 330px;
   border-radius: 5px 5px 0 0;
 
   &:hover ${Wrapper} {
-    filter: brightness(0.9775);
     box-shadow: 0 6px 0 0 #fcd420;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
       box-shadow: 0 4px 0 0 #fcd420;
     }
   }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-    height: 230px;
-  }
 `;
 
 export const Image = styled.img`
   //example sizes
   background: darkgray;
-  height: 80%;
+  height: 295px;
   border-radius: 5px 5px 0 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    height: 195px;
+  }
 `;
 
-export const Name = styled.h3`
-  font-size: 20px;
+export const Name = styled.span`
+  font-size: 18px;
   font-weight: 500;
-  margin: 0;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     font-size: 14px;
+  }
+`;
+
+export const Role = styled.span`
+  font-size: 16px;
+  color: #7e839a;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 12px;
   }
 `;

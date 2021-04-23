26fa8b81856c0f42a1941b69894ae045bc295003
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-23 17:13:30 +0200

    separated Tile from structure

--- src/App/features/moviePage/MovieMain/index.js
+++ src/App/features/moviePage/MovieMain/index.js
@@ -1,6 +1,3 @@
-import Subheader from '../../../common/styled/Subheader';
-import Genres from '../../../common/structure/Genres';
-import Section from '../../../common/styled/Section';
 import {
   Container,
   Wallpaper,
@@ -13,81 +10,26 @@ import {
   WallpaperContainer,
   WallpaperFill,
   Details,
-  Poster,
-  TileWrapper,
-  StyledMovieTile,
-  Year,
-  InformationsWrapper,
-  Informations,
-  Rating,
-  SmallStarIcon,
-  TileRate,
-  TileRateExtra,
-  Description,
-  Information,
-  InformationDetails,
 } from './styled';
 
-const MovieMain = ({
-  movieWallpaper,
-  title,
-  rate,
-  votes,
-  year,
-  genres,
-  production,
-  date,
-  description,
-}) => (
-  <>
-    <WallpaperFill>
-      <WallpaperContainer>
-        <WallpaperOverlay />
-        <Wallpaper alt={`"${title}" movie wallpaper`} src={movieWallpaper} />
-        <Wrapper>
-          <Title>{title}</Title>
-          <Details>
-            <Container>
-              <StarIcon />
-              <Rate>{rate}</Rate>
-              <TextContent>/&nbsp;10</TextContent>
-            </Container>
-            <TextContent>{votes}&nbsp;votes</TextContent>
-          </Details>
-        </Wrapper>
-      </WallpaperContainer>
-    </WallpaperFill>
-    <Section>
-      <StyledMovieTile>
-        <Poster
-          alt={`"${title}" movie poster`}
-          src={'https://thisdoesnotexist.com/'}
-        />
-        <TileWrapper>
-          <Subheader>{title}</Subheader>
-          <Year>{year}</Year>
-          <InformationsWrapper>
-            <Informations>
-              <Information>Production:</Information>
-              <InformationDetails>{production}</InformationDetails>
-            </Informations>
-            <Informations>
-              <Information>Release&nbsp;date:</Information>
-              <InformationDetails>{date}</InformationDetails>
-            </Informations>
-          </InformationsWrapper>
-          <Genres genres={genres} />
-          <Rating>
-            <SmallStarIcon />
-            <TileRate>{rate}</TileRate>
-            <TileRateExtra>/&nbsp;10</TileRateExtra>
-            {votes}&nbsp;votes
-          </Rating>
-        </TileWrapper>
-        <Description>{description}</Description>
-      </StyledMovieTile>
-    </Section>
-  </>
+const MovieMain = ({ movieWallpaper, title, rate, votes }) => (
+  <WallpaperFill>
+    <WallpaperContainer>
+      <WallpaperOverlay />
+      <Wallpaper alt={`"${title}" movie wallpaper`} src={movieWallpaper} />
+      <Wrapper>
+        <Title>{title}</Title>
+        <Details>
+          <Container>
+            <StarIcon />
+            <Rate>{rate}</Rate>
+            <TextContent>/&nbsp;10</TextContent>
+          </Container>
+          <TextContent>{votes}&nbsp;votes</TextContent>
+        </Details>
+      </Wrapper>
+    </WallpaperContainer>
+  </WallpaperFill>
 );
 
 export default MovieMain;

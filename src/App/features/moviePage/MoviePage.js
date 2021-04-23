fa7c4d25f9c36215c48018d315197766648e4b6f
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-23 17:15:53 +0200

    added Tile to structure and added date object to take repeatable props

--- src/App/features/moviePage/MoviePage.js
+++ src/App/features/moviePage/MoviePage.js
@@ -1,27 +1,40 @@
 import MovieMain from './MovieMain';
-import movieWallpaper from '../.././images/example/movieWallpaper.jpg';
 import Subheader from '../../common/styled/Subheader';
+import Tile from '../../common/structure/Tile';
 import List from '../../common/styled/List';
 import Section from '../../common/styled/Section';
 import Person from '../../common/structure/Person';
+import movieWallpaper from '../.././images/example/movieWallpaper.jpg';
 
 //test structure
+const date = {
+  title: 'Mulan',
+  rate: '7,8',
+  votes: '35',
+};
 
 const MoviePage = () => (
   <>
     <MovieMain
       movieWallpaper={movieWallpaper}
-      title={'Mulan'}
-      rate={'7,8'}
-      votes={'35'}
-      year={'2020'}
-      genres={'Action'}
-      production={'China, United States of America'}
-      date={'24.10.2020'}
-      description={
-        'A young Chinese maiden disguises herself as a male warrior in order to save her father. Disguises herself as a male warrior in order to save her father. A young Chinese maiden disguises herself as a male warrior in order to save her father.'
-      }
+      title={date.title}
+      rate={date.rate}
+      votes={date.votes}
     />
+    <Section>
+      <Tile
+        title={date.title}
+        rate={date.rate}
+        votes={date.votes}
+        year={'2020'}
+        genres={'Action'}
+        production={'China, United States of America'}
+        date={'24.10.2020'}
+        description={
+          'A young Chinese maiden disguises herself as a male warrior in order to save her father. Disguises herself as a male warrior in order to save her father. A young Chinese maiden disguises herself as a male warrior in order to save her father.'
+        }
+      />
+    </Section>
     <Section>
       <Subheader>Cast</Subheader>
       <List people>

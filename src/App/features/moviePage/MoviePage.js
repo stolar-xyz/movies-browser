239c72737b5c28c6dd3e69a28fa0b4a7e544b255
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-25 18:33:40 +0200

    passed Rating component in rating prop

--- src/App/features/moviePage/MoviePage.js
+++ src/App/features/moviePage/MoviePage.js
@@ -5,12 +5,13 @@ import List from '../../common/styled/List';
 import Section from '../../common/styled/Section';
 import Person from '../../common/structure/Person';
 import movieWallpaper from '../.././images/example/movieWallpaper.jpg';
+import Rating from '../../common/structure/Rating';
 
 //test structure
 const date = {
   title: 'Mulan',
   rate: '7,8',
-  votes: '35',
+  votes: '355',
 };
 
 const MoviePage = () => (
@@ -18,18 +19,15 @@ const MoviePage = () => (
     <MovieWallpaper
       movieWallpaper={movieWallpaper}
       title={date.title}
-      rate={date.rate}
-      votes={date.votes}
+      rating={<Rating big rate={date.rate} votes={date.votes} />}
     />
     <Section>
       <Tile
         title={date.title}
-        rate={date.rate}
-        votes={date.votes}
         year={'2020'}
-        genres={'Action'}
         production={'China, United States of America'}
         date={'24.10.2020'}
+        rating={<Rating rate={date.rate} votes={date.votes} />}
         description={
           'A young Chinese maiden disguises herself as a male warrior in order to save her father. Disguises herself as a male warrior in order to save her father. A young Chinese maiden disguises herself as a male warrior in order to save her father.'
         }

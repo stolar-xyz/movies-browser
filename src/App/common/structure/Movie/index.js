1e00f768d429f01cc6c50da39255149cacfcb228
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-26 15:37:49 +0200

    deleted unused styled components and added Rating to structure

--- src/App/common/structure/Movie/index.js
+++ src/App/common/structure/Movie/index.js
@@ -1,16 +1,6 @@
 import Genres from '../Genres';
-import {
-  StyledMovie,
-  Image,
-  Rate,
-  Votes,
-  Rating,
-  StarIcon,
-  Title,
-  Year,
-  Wrapper,
-  Container,
-} from './styled';
+import Rating from '../Rating';
+import { StyledMovie, Image, Title, Year, Wrapper, Container } from './styled';
 
 const Movie = ({ title, year, genres, rate, votes }) => (
   <StyledMovie>
@@ -24,11 +14,7 @@ const Movie = ({ title, year, genres, rate, votes }) => (
         <Year>{year}</Year>
         <Genres genres={genres} />
       </Container>
-      <Rating>
-        <StarIcon />
-        <Rate>{rate}</Rate>
-        <Votes>{votes}&nbsp;votes</Votes>
-      </Rating>
+      <Rating rate={rate} votes={votes} />
     </Wrapper>
   </StyledMovie>
 );

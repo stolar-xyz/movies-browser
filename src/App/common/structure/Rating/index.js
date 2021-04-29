7edb0e6f8aa82661624218d785e7446357bc3fd3
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-29 14:55:48 +0200

    deleted Container from structure and added big prop to Votes

--- src/App/common/structure/Rating/index.js
+++ src/App/common/structure/Rating/index.js
@@ -1,24 +1,15 @@
-import {
-  Container,
-  Limit,
-  Rate,
-  StarIcon,
-  StyledRating,
-  Votes,
-} from './styled';
+import { Limit, Rate, StarIcon, StyledRating, Votes } from './styled';
 
 const Rating = ({ rate, votes, big, small }) => (
   <StyledRating big={big}>
-    <Container big={big}>
-      <StarIcon big={big} />
-      <Rate big={big} small={small}>
-        {rate}
-      </Rate>
-      <Limit big={big}>
-        /&nbsp;10
-      </Limit>
-    </Container>
-    <Votes small={small}>{votes}&nbsp;votes</Votes>
+    <StarIcon big={big} />
+    <Rate big={big} small={small}>
+      {rate}
+    </Rate>
+    <Limit big={big}>/&nbsp;10</Limit>
+    <Votes big={big} small={small}>
+      {votes}&nbsp;votes
+    </Votes>
   </StyledRating>
 );
 

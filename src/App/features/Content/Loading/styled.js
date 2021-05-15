cfcc1eb7c0c0363577bbc6ef65933dc722e21129
Andrzej Stolarek <stolar.code@gmail.com>
2021-05-15 15:25:37 +0200

    added relative values ​​to Loader sizes

--- src/App/features/Content/Loading/styled.js
+++ src/App/features/Content/Loading/styled.js
@@ -7,16 +7,14 @@ const spin = keyframes`
 `;
 
 export const Loader = styled.div`
-  width: 160px;
-  height: 160px;
+  width: 12vw;
+  height: 12vw;
   border-radius: 50%;
   border: 10px solid #dddeee;
   border-top-color: #000000;
   animation: ${spin} 1s ease-in-out infinite;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-    width: 80px;
-    height: 80px;
     border-width: 5px;
   }
 `;

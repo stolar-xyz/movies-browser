857f80495338e54e66ddc79db6bbd3adad3813dd
Andrzej Stolarek <stolar.code@gmail.com>
2021-05-19 22:50:30 +0200

    added primary colors to theme

--- src/App/theme.js
+++ src/App/theme.js
@@ -4,6 +4,46 @@ const breakpoints = {
   tabletMax: '1023',
 };
 
+const colors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  whisper: '#F5F5FA',
+  stormGray: '#717487',
+  waterloo: '#7E839A',
+  scienceBlue: '#0044CC',
+  mystic: '#E4E6F0',
+  pattensBlue: '#D6E4FF',
+  candlelight: '#FCD420',
+};
+
 export const theme = {
+  color: {
+    text: {
+      primaryText: colors.black,
+      secondaryText: colors.white,
+      extraText: colors.stormGray,
+    },
+
+    background: {
+      primaryBackground: colors.whisper,
+      secondaryBackground: colors.white,
+      genre: colors.mystic,
+      failureButton: colors.scienceBlue,
+      loader: colors.mystic,
+    },
+
+    element: {
+      arrowheadActive: colors.scienceBlue,
+      arrowheadDisabled: colors.waterloo,
+      pageSelectButtonActive: colors.pattensBlue,
+      pageSelectButtonDisabled: colors.mystic,
+    },
+
+    hover: {
+      boxBottomShadow: colors.candlelight,
+      pageSelectBorder: colors.scienceBlue,
+    },
+  },
+
   ...breakpoints,
 };

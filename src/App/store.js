d38559ca36cbb40762f805c14ac58cd8dd326850
Andrzej Stolarek <stolar.code@gmail.com>
2021-05-29 23:13:43 +0200

    passed themeReducer to store

--- src/App/store.js
+++ src/App/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './rootSaga';
+import themeReducer from './features/themeToggler/themeSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    theme: themeReducer,
+  },
   middleware: [sagaMiddleware],
 });
 

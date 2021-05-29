01fcfaff9700de440c666145e212e76e8cbbb2d2
Andrzej Stolarek <stolar.code@gmail.com>
2021-05-29 23:22:27 +0200

    changed ThemeToggler structure

--- src/App/features/themeToggler/ThemeToggler.js
+++ src/App/features/themeToggler/ThemeToggler.js
@@ -1,8 +1,17 @@
 import { Toggler, Ball, TogglerField } from './styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectTheme, toggleTheme } from './themeSlice';
 
 const ThemeToggler = () => {
+  const isTheme = useSelector(selectTheme);
+  const dispatch = useDispatch();
+
   return (
-    <Toggler title={'Toggle theme'}>
+    <Toggler
+      onClick={() => dispatch(toggleTheme())}
+      clicked={isTheme}
+      title={'Toggle theme'}
+    >
       <TogglerField>
         <Ball />
       </TogglerField>

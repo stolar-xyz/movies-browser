ca0367c7b493c1676a0a85e151b64e4a49fc8c5f
Andrzej Stolarek <stolar.code@gmail.com>
2021-05-29 23:14:22 +0200

    passed themeSaga to rootSaga

--- src/App/rootSaga.js
+++ src/App/rootSaga.js
@@ -1,5 +1,6 @@
 import { all } from 'redux-saga/effects';
+import { themeSaga } from './features/themeToggler/themeSaga';
 
 export default function* rootSaga() {
-  yield all([]);
+  yield all([themeSaga()]);
 }

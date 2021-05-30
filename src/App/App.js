23872b2b7be933cc7b75c0565c2426684fe700d2
Andrzej Stolarek <stolar.code@gmail.com>
2021-05-30 20:10:32 +0200

    passed darkTheme and lightTheme to App structure depending on the constant

--- src/App/App.js
+++ src/App/App.js
@@ -7,34 +7,40 @@ import PersonPage from './features/personPage/PersonPage';
 import { Normalize } from 'styled-normalize';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
+import { darkTheme, lightTheme } from './theme';
 import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { toMovies, toPeople } from './routes';
+import { useSelector } from 'react-redux';
+import { selectTheme } from './features/themeToggler/themeSlice';
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Normalize />
-    <HashRouter>
-      <Navigation />
-      <main>
-        <Switch>
-          <Route path={toMovies()}>
-            <MoviePage />
-            <Movies />
-          </Route>
-          <Route path={toPeople()}>
-            <PersonPage />
-            <People />
-          </Route>
-          <Route>
-            <Redirect to={toMovies()} />
-          </Route>
-        </Switch>
-      </main>
-    </HashRouter>
-    <PageSelect minNumber={'1'} maxNumber={'500'} />
-  </ThemeProvider>
-);
+const App = () => {
+  const isTheme = useSelector(selectTheme);
+
+  return (
+    <ThemeProvider theme={isTheme ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <Normalize />
+      <HashRouter>
+        <Navigation />
+        <main>
+          <Switch>
+            <Route path={toMovies()}>
+              <MoviePage />
+              <Movies />
+            </Route>
+            <Route path={toPeople()}>
+              <PersonPage />
+              <People />
+            </Route>
+            <Route>
+              <Redirect to={toMovies()} />
+            </Route>
+          </Switch>
+        </main>
+      </HashRouter>
+      <PageSelect minNumber={'1'} maxNumber={'500'} />
+    </ThemeProvider>
+  );
+};
 
 export default App;

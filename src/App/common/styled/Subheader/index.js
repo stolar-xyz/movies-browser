9658cee1327c97cddbfb95f276ecfb18fce29c94
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-04 22:44:30 +0200

    added extra prop to Subheader

--- src/App/common/styled/Subheader/index.js
+++ src/App/common/styled/Subheader/index.js
@@ -1,10 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Subheader = styled.h2`
   color: ${({ theme }) => theme.color.text.primaryText};
   font-size: 36px;
   font-weight: 600;
   margin: 0;
+
+  ${({ extra }) =>
+    extra &&
+    css`
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    `}
 
   @media (max-width: ${({ theme }) => theme.mobileMax}px) {
     font-size: 18px;

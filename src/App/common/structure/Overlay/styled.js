8b4cff2fea7a1489bc79c2ab05d958495f7a32c6
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-08 18:59:06 +0200

    added StyledOverlay component

---
+++ src/App/common/structure/Overlay/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledOverlay = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #c4c4c4;
+  height: 100%;
+`;

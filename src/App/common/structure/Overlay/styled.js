8755c0d16cf44defd74eecafc35ad5a3ab605842
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-09 23:09:17 +0200

    changed StyledOverlay styles depending on props

--- src/App/common/structure/Overlay/styled.js
+++ src/App/common/structure/Overlay/styled.js
@@ -1,9 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const StyledOverlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   background: #c4c4c4;
-  height: 100%;
+
+  ${({ person }) =>
+    person &&
+    css`
+      height: 260px;
+
+      @media (max-width: ${({ theme }) => theme.mobileMax}px) {
+        height: 195px;
+      }
+    `}
+
+  ${({ movie }) =>
+    movie &&
+    css`
+      height: 100%;
+
+      @media (max-width: ${({ theme }) => theme.mobileMax}px) {
+        height: 220px;
+        min-width: 145px;
+      }
+    `}
 `;

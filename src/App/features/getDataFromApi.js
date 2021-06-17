e5a8e514f800f6939f975733a66803e73e583637
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-17 20:15:25 +0200

    added getDataFromApi file

---
+++ src/App/features/getDataFromApi.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const getDataFromApi = async url => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.log('error', error);
+  }
+};

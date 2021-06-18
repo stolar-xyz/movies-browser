d56c9d9226be7b973b8eb1dab7358314eaff6ee2
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-18 22:03:38 +0200

    passed listSlice to store

--- src/App/store.js
+++ src/App/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './rootSaga';
 import themeReducer from './features/themeToggler/themeSlice';
+import listReducer from './features/listSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
+    list: listReducer,
   },
   middleware: [sagaMiddleware],
 });

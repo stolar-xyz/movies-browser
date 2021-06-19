5b1a83d349f8af4b1e130849ba769cf0eb4092d8
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-19 20:22:54 +0200

    deleted MoviePage and PersonPage from App structure (previously added for the test)

--- src/App/App.js
+++ src/App/App.js
@@ -2,8 +2,6 @@ import Navigation from './common/structure/Navigation';
 import Movies from './features/movies/Movies';
 import People from './features/people/People';
 import PageSelect from './common/structure/PageSelect';
-import MoviePage from './features/moviePage/MoviePage';
-import PersonPage from './features/personPage/PersonPage';
 import { Normalize } from 'styled-normalize';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
@@ -25,11 +23,9 @@ const App = () => {
         <main>
           <Switch>
             <Route path={toMovies()}>
-              <MoviePage />
               <Movies />
             </Route>
             <Route path={toPeople()}>
-              <PersonPage />
               <People />
             </Route>
             <Route>

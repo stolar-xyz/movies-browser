c0b3c556cfaee7743145f08950eab54395e99d5e
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-19 20:19:48 +0200

    added baseImage value

--- src/App/apiDetails.js
+++ src/App/apiDetails.js
@@ -1,3 +1,4 @@
 export const base = 'https://api.themoviedb.org/3/';
+export const baseImage = 'https://image.tmdb.org/t/p/';
 export const apiKey = '?api_key=6675134d019c190f28bd0c6e0f5fa814';
 export const language = '&language=en-US';

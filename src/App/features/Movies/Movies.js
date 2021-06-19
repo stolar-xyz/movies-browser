dad25313fc1375a5b0c8d87518584e1e97aad55c
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-19 21:00:31 +0200

    deleted test structure and added mapping from resultPage which is holding the response from the api

--- src/App/features/Movies/Movies.js
+++ src/App/features/Movies/Movies.js
@@ -1,49 +1,49 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchList, selectResult } from '../listSlice';
 import Subheader from '../../common/styled/Subheader';
 import Movie from '../../common/structure/Movie';
 import Section from '../../common/styled/Section';
 import List from '../../common/styled/List';
 import ThemeToggler from '../themeToggler/ThemeToggler';
-import moviePoster from '../.././assets/example/moviePoster.jpg';
 
-const Movies = () => (
-  <Section>
-    <Subheader extra>
-      Popular movies <ThemeToggler />
-    </Subheader>
-    <List movies>
-      <Movie
-        title={'Mulan'}
-        year={'2020'}
-        genres={'Action'}
-        rate={'7,8'}
-        votes={'35'}
-        source={moviePoster}
-      />
-      <Movie
-        title={'Mulan'}
-        year={'2020'}
-        genres={'Action'}
-        rate={'7,8'}
-        votes={'35'}
-      />
-      <Movie
-        title={'Mulan'}
-        year={'2020'}
-        genres={'Action'}
-        rate={'7,8'}
-        votes={'35'}
-        source={moviePoster}
-      />
-      <Movie
-        title={'Mulan'}
-        year={'2020'}
-        genres={'Action'}
-        rate={'7,8'}
-        votes={'35'}
-        source={moviePoster}
-      />
-    </List>
-  </Section>
-);
+const Movies = () => {
+  const resultPage = useSelector(selectResult);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchList({ page: '1', type: 'movies' }));
+  }, [dispatch]);
+
+  return (
+    <Section>
+      <Subheader extra>
+        Popular movies <ThemeToggler />
+      </Subheader>
+      <List movies>
+        {resultPage.map(
+          ({
+            title,
+            release_date,
+            vote_average,
+            vote_count,
+            poster_path,
+            id,
+          }) => (
+            <Movie
+              title={title}
+              year={release_date} //incorrect date (same year)
+              genres={'Action'} //test
+              rate={vote_average}
+              votes={vote_count}
+              source={poster_path}
+              key={id}
+            />
+          )
+        )}
+      </List>
+    </Section>
+  );
+};
 
 export default Movies;
--- src/App/features/People/People.js
+++ src/App/features/People/People.js
@@ -1,28 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchList, selectResult } from '../listSlice';
 import Subheader from '../../common/styled/Subheader';
 import Person from '../../common/structure/Person';
 import Section from '../../common/styled/Section';
 import List from '../../common/styled/List';
 import ThemeToggler from '../themeToggler/ThemeToggler';
 
-//test People structure
+const People = () => {
+  const resultPage = useSelector(selectResult);
+  const dispatch = useDispatch();
 
-const People = () => (
-  <Section>
-    <Subheader extra>
-      Popular people <ThemeToggler />
-    </Subheader>
-    <List people>
-      <Person name={'Henry Cavill'} source={''}/>
-      <Person name={'Henry Cavill'} />
-      <Person name={'Henry Cavill'} />
-      <Person name={'Henry Cavill'} />
-      <Person name={'Henry Cavill'} />
-      <Person name={'Henry Cavill'} />
-      <Person name={'Henry Cavill'} />
-      <Person name={'Henry Cavill'} />
-      <Person name={'Henry Cavill'} />
-    </List>
-  </Section>
-);
+  useEffect(() => {
+    dispatch(fetchList({ page: '1', type: 'people' }));
+  }, [dispatch]);
+
+  return (
+    <Section>
+      <Subheader extra>
+        Popular people <ThemeToggler />
+      </Subheader>
+      <List people>
+        {resultPage.map(({ name, profile_path, id }) => (
+          <Person name={name} source={profile_path} key={id} />
+        ))}
+      </List>
+    </Section>
+  );
+};
 
 export default People;

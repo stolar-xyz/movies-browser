a64e63236ff8828fb01c0a473786928abe9fcee2
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-19 21:10:32 +0200

    deleted example structure

--- src/App/common/structure/Genres/index.js
+++ src/App/common/structure/Genres/index.js
@@ -1,12 +1,8 @@
 import { Genre, StyledGenres } from './styled';
 
 const Genres = ({ genres }) => (
-  //example values
   <StyledGenres>
     <Genre>{genres}</Genre>
-    <Genre>Adventure</Genre>
-    <Genre>Drama</Genre>
-    <Genre>Horror</Genre>
   </StyledGenres>
 );
 

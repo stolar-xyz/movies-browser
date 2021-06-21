1e94c9c3e34577b16b5a7e47ccb4aac3347123f0
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-21 18:25:37 +0200

    added genresSaga to rootSaga

--- src/App/rootSaga.js
+++ src/App/rootSaga.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
+import { genresSaga } from './common/structure/Genres/genresSaga';
 import { listSaga } from './features/listSaga';
 import { themeSaga } from './features/themeToggler/themeSaga';
 
 export default function* rootSaga() {
-  yield all([themeSaga(), listSaga()]);
+  yield all([themeSaga(), listSaga(), genresSaga()]);
 }

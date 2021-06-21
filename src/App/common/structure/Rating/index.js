a413fba3e34faf97f18a12ce4969a6e15cfb793a
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-21 19:22:33 +0200

    added ternary operator depending on votes prop

--- src/App/common/structure/Rating/index.js
+++ src/App/common/structure/Rating/index.js
@@ -2,14 +2,20 @@ import { Limit, Rate, StarIcon, StyledRating, Votes } from './styled';
 
 const Rating = ({ rate, votes, big, small }) => (
   <StyledRating big={big} small={small}>
-    <StarIcon big={big} />
-    <Rate big={big} small={small}>
-      {rate}
-    </Rate>
-    <Limit big={big}>/&nbsp;10</Limit>
-    <Votes big={big} small={small}>
-      {votes}&nbsp;votes
-    </Votes>
+    {votes ? (
+      <>
+        <StarIcon big={big} />
+        <Rate big={big} small={small}>
+          {rate}
+        </Rate>
+        <Limit big={big}>/&nbsp;10</Limit>
+        <Votes big={big} small={small}>
+          {votes}&nbsp;votes
+        </Votes>
+      </>
+    ) : (
+      <Votes small={small}>No votes yet</Votes>
+    )}
   </StyledRating>
 );
 

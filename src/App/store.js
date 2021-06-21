7759ff44e30f14a7d354b1d08af1c94b9e7f01e7
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-21 18:25:23 +0200

    added genresReducer to store

--- src/App/store.js
+++ src/App/store.js
@@ -3,6 +3,7 @@ import createSagaMiddleware from 'redux-saga';
 import rootSaga from './rootSaga';
 import themeReducer from './features/themeToggler/themeSlice';
 import listReducer from './features/listSlice';
+import genresReducer from './common/structure/Genres/genresSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -10,6 +11,7 @@ const store = configureStore({
   reducer: {
     theme: themeReducer,
     list: listReducer,
+    genres: genresReducer,
   },
   middleware: [sagaMiddleware],
 });

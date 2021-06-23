26ad318d1b9df46acd47c8d693637ee00f5c2f8f
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-23 17:21:38 +0200

    added render depending on listStatus

--- src/App/features/movies/MoviesPage.js
+++ src/App/features/movies/MoviesPage.js
@@ -1,15 +1,18 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchList, selectResult } from '../listSlice';
+import { fetchList, selectResult, selectStatus } from '../listSlice';
 import { fetchGenres } from './Genres/genresSlice';
 import Subheader from '../../common/styled/Subheader';
 import Movie from '../../common/structure/Movie';
 import Section from '../../common/styled/Section';
 import List from '../../common/styled/List';
 import ThemeToggler from '../ThemeToggler';
+import Loading from '../../common/structure/Content/Loading';
+import Failure from '../../common/structure/Content/Failure';
 
 const MoviesPage = () => {
   const resultPage = useSelector(selectResult);
+  const listStatus = useSelector(selectStatus);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -17,37 +20,52 @@ const MoviesPage = () => {
     dispatch(fetchGenres());
   }, [dispatch]);
 
-  return (
-    <Section>
-      <Subheader extra>
-        Popular movies
-        <ThemeToggler />
-      </Subheader>
-      <List movies>
-        {resultPage.map(
-          ({
-            title,
-            release_date,
-            genre_ids,
-            vote_average,
-            vote_count,
-            poster_path,
-            id,
-          }) => (
-            <Movie
-              title={title}
-              year={release_date}
-              genres={genre_ids}
-              rate={vote_average}
-              votes={vote_count}
-              source={poster_path}
-              key={id}
-            />
-          )
-        )}
-      </List>
-    </Section>
-  );
+  switch (listStatus) {
+    case 'initial':
+      return null;
+
+    case 'loading':
+      return <Loading />;
+
+    case 'error':
+      return <Failure />;
+
+    case 'success':
+      return (
+        <Section>
+          <Subheader extra>
+            Popular movies
+            <ThemeToggler />
+          </Subheader>
+          <List movies>
+            {resultPage.map(
+              ({
+                title,
+                release_date,
+                genre_ids,
+                vote_average,
+                vote_count,
+                poster_path,
+                id,
+              }) => (
+                <Movie
+                  title={title}
+                  year={release_date}
+                  genres={genre_ids}
+                  rate={vote_average}
+                  votes={vote_count}
+                  source={poster_path}
+                  key={id}
+                />
+              )
+            )}
+          </List>
+        </Section>
+      );
+
+    default:
+      throw new Error(`incorrect status - ${listStatus}`);
+  }
 };
 
 export default MoviesPage;

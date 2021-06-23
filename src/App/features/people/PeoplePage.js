a2ed52598b25d5efba3cf505f7cf62fba47bd42b
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-23 17:29:32 +0200

    added render depending on listStatus

--- src/App/features/people/PeoplePage.js
+++ src/App/features/people/PeoplePage.js
@@ -1,33 +1,51 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchList, selectResult } from '../listSlice';
+import { fetchList, selectResult, selectStatus } from '../listSlice';
 import Subheader from '../../common/styled/Subheader';
 import Person from '../../common/structure/Person';
 import Section from '../../common/styled/Section';
 import List from '../../common/styled/List';
 import ThemeToggler from '../ThemeToggler';
+import Loading from '../../common/structure/Content/Loading';
+import Failure from '../../common/structure/Content/Failure';
 
 const PeoplePage = () => {
   const resultPage = useSelector(selectResult);
+  const listStatus = useSelector(selectStatus);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchList({ page: '1', type: 'people' }));
   }, [dispatch]);
 
-  return (
-    <Section>
-      <Subheader extra>
-        Popular people
-        <ThemeToggler />
-      </Subheader>
-      <List people>
-        {resultPage.map(({ name, profile_path, id }) => (
-          <Person name={name} source={profile_path} key={id} />
-        ))}
-      </List>
-    </Section>
-  );
+  switch (listStatus) {
+    case 'initial':
+      return null;
+
+    case 'loading':
+      return <Loading />;
+
+    case 'error':
+      return <Failure />;
+
+    case 'success':
+      return (
+        <Section>
+          <Subheader extra>
+            Popular people
+            <ThemeToggler />
+          </Subheader>
+          <List people>
+            {resultPage.map(({ name, profile_path, id }) => (
+              <Person name={name} source={profile_path} key={id} />
+            ))}
+          </List>
+        </Section>
+      );
+
+    default:
+      throw new Error(`incorrect status - ${listStatus}`);
+  }
 };
 
 export default PeoplePage;

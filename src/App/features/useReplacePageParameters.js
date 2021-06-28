3ed16071dff40a0502e525a31f96e870f35017b9
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-28 18:45:28 +0200

    added useReplacePageParameters file

---
+++ src/App/features/useReplacePageParameters.js
@@ -0,0 +1,13 @@
+import { useHistory } from 'react-router-dom';
+
+export const useReplacePageParameters = () => {
+  const history = useHistory();
+  const searchParams = new URLSearchParams();
+
+  const replacePageParameters = ({ key, value }) => {
+    searchParams.set(key, value);
+    history.push(`?${searchParams}`);
+  };
+
+  return replacePageParameters;
+};

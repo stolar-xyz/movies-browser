0c680c73bc89779d06b2b5922457469afa9b8fd7
Andrzej Stolarek <stolar.code@gmail.com>
2021-06-28 19:02:59 +0200

    changed PageSelect structure by adding relative values and functions

--- src/App/common/structure/PageSelect/index.js
+++ src/App/common/structure/PageSelect/index.js
@@ -1,3 +1,8 @@
+import { useSelector } from 'react-redux';
+import { selectTotalPages } from '../../../features/listSlice';
+import { usePageParameters } from '../../../features/usePageParameters';
+import { useReplacePageParameters } from '../../../features/useReplacePageParameters';
+import { pageKey } from '../../../paramKeys';
 import {
   ArrowheadIcon,
   ArrowheadsWrapper,
@@ -9,37 +14,63 @@ import {
   Wrapper,
 } from './styled';
 
-const PageSelect = ({ minNumberPage, maxNumberPage }) => (
-  <StyledPageSelect>
-    <PageButton disabled title={'First page'}>
-      <ArrowheadsWrapper>
+const PageSelect = () => {
+  const totalPages = useSelector(selectTotalPages);
+  const page = +usePageParameters();
+  const replacePageParameters = useReplacePageParameters();
+
+  const onButtonClick = page => {
+    replacePageParameters({ key: pageKey, value: page });
+  };
+
+  return (
+    <StyledPageSelect>
+      <PageButton
+        disabled={page === 1}
+        onClick={() => onButtonClick(1)}
+        title={'First page'}
+      >
+        <ArrowheadsWrapper>
+          <ArrowheadIcon />
+          <ArrowheadIcon />
+        </ArrowheadsWrapper>
+        <PageButtonText>First</PageButtonText>
+      </PageButton>
+      <PageButton
+        disabled={page === 1}
+        onClick={() => onButtonClick(page - 1)}
+        title={'Previous page'}
+      >
         <ArrowheadIcon />
-        <ArrowheadIcon />
-      </ArrowheadsWrapper>
-      <PageButtonText>First</PageButtonText>
-    </PageButton>
-    <PageButton disabled title={'Previous page'}>
-      <ArrowheadIcon />
-      <PageButtonText>Previous</PageButtonText>
-    </PageButton>
-    <Wrapper>
-      <PageText>Page</PageText>
-      <PageNumber>{minNumberPage}</PageNumber>
-      <PageText>of</PageText>
-      <PageNumber>{maxNumberPage}</PageNumber>
-    </Wrapper>
-    <PageButton title={'Next page'}>
-      <PageButtonText>Next</PageButtonText>
-      <ArrowheadIcon rotated={'true'} />
-    </PageButton>
-    <PageButton title={'Last page'}>
-      <PageButtonText>Last</PageButtonText>
-      <ArrowheadsWrapper>
+        <PageButtonText>Previous</PageButtonText>
+      </PageButton>
+      <Wrapper>
+        <PageText>Page</PageText>
+        <PageNumber>{page}</PageNumber>
+        <PageText>of</PageText>
+        <PageNumber>{totalPages}</PageNumber>
+      </Wrapper>
+      <PageButton
+        disabled={page === totalPages}
+        onClick={() => onButtonClick(page + 1)}
+        title={'Next page'}
+      >
+        <PageButtonText>Next</PageButtonText>
         <ArrowheadIcon rotated={'true'} />
-        <ArrowheadIcon rotated={'true'} />
-      </ArrowheadsWrapper>
-    </PageButton>
-  </StyledPageSelect>
-);
+      </PageButton>
+      <PageButton
+        disabled={page === totalPages}
+        onClick={() => onButtonClick(totalPages)}
+        title={'Last page'}
+      >
+        <PageButtonText>Last</PageButtonText>
+        <ArrowheadsWrapper>
+          <ArrowheadIcon rotated={'true'} />
+          <ArrowheadIcon rotated={'true'} />
+        </ArrowheadsWrapper>
+      </PageButton>
+    </StyledPageSelect>
+  );
+};
 
 export default PageSelect;

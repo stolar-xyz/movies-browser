3c31cf049f9a51f684cce535fd677222a9608f19
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-04 13:08:41 +0200

    added searchSlice file

---
+++ src/App/features/Search/searchSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const searchSlice = createSlice({
+  name: 'search',
+  initialState: {
+    results: [],
+    activePath: '',
+    query: '',
+    status: 'initial',
+  },
+  reducers: {
+    fetchSearch: state => {
+      state.status = 'loading';
+    },
+    fetchSearchSuccess: (state, { payload }) => {
+      state.results = payload.results;
+      state.status = 'success';
+    },
+    fetchSearchError: state => {
+      state.status = 'error';
+    },
+    setActiveSearchPath: (state, { payload: path }) => {
+      state.activePath = path;
+      state.status = 'loading';
+    },
+    setQuery: (state, { payload: query }) => {
+      state.query = query;
+    },
+  },
+});
+
+export const {
+  fetchSearch,
+  fetchSearchSuccess,
+  fetchSearchError,
+  setActiveSearchPath,
+  setQuery,
+  setOpen,
+} = searchSlice.actions;
+
+export const selectResults = state => state.search.results;
+export const selectQuery = state => state.search.query;
+export const selectStatus = state => state.search.loading;
+
+export default searchSlice.reducer;

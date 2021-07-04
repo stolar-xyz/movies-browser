b9d067cb7de0ae13be2a600c004dcc87cdd9d50b
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-04 13:14:47 +0200

    added onChange function to input and passed query to value

--- src/App/features/Search/index.js
+++ src/App/features/Search/index.js
@@ -1,11 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { selectQuery, setQuery } from './searchSlice';
 import { StyledSearch, Button, Label, SearchIcon, Field } from './styled';
 
 const Search = () => {
+  const dispatch = useDispatch();
+  const query = useSelector(selectQuery);
   const { pathname } = useLocation();
 
   const onFormSubmit = event => {
     event.preventDefault();
+  };
+
+  const onChange = value => {
+    dispatch(setQuery(value));
   };
 
   return (
@@ -15,6 +23,8 @@ const Search = () => {
       </Button>
       <Label>
         <Field
+          value={query}
+          onChange={({ target }) => onChange(target.value)}
           placeholder={`Search for ${
             pathname.includes('movies') ? 'movies' : 'people'
           }...`}

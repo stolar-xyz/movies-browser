1d93cbc0d3134f82f28e9ee3053df9a1577165d7
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-07 14:48:07 +0200

    changed title prop to text

--- src/App/common/structure/Content/NoResults/index.js
+++ src/App/common/structure/Content/NoResults/index.js
@@ -2,9 +2,9 @@ import Container from '../../../styled/Container';
 import Subheader from '../../../styled/Subheader';
 import { NoResultsPicture } from './styled';
 
-const NoResults = ({ title }) => (
+const NoResults = ({ text }) => (
   <>
-    <Subheader>Sorry, there are no results for “{title}”</Subheader>
+    <Subheader>Sorry, there are no results for "{text}"</Subheader>
     <Container>
       <NoResultsPicture />
     </Container>

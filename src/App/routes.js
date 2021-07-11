6b925ceb9d6db9dce7c14542da3c39c6e598e4a2
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-11 22:22:43 +0200

    added toMovie and toPerson to routes

--- src/App/routes.js
+++ src/App/routes.js
@@ -1,2 +1,4 @@
 export const toMovies = () => '/movies';
 export const toPeople = () => '/people';
+export const toMovie = id => `/movies/movie/${id}`;
+export const toPerson = id => `/people/person/${id}`;

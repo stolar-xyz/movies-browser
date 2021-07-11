0cc724a52b36140c800da092a63ead13d10f2e39
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-11 22:24:05 +0200

    passed toMovie and toPerson to ResultTitle component

--- src/App/features/Search/ResultsBox/ResultTile/index.js
+++ src/App/features/Search/ResultsBox/ResultTile/index.js
@@ -1,10 +1,9 @@
 import { baseImage } from '../../../../apiDetails';
+import { toMovie, toPerson } from '../../../../routes';
 import { Image, Text, Wrapper } from './styled';
 
 const ResultTile = ({ pathname, id, source, text }) => (
-  <Wrapper
-    to={pathname === 'movie' ? `/movies/movie/${id}` : `/people/person/${id}`}
-  >
+  <Wrapper to={pathname === 'movie' ? toMovie(id) : toPerson(id)}>
     <Image alt={text} src={`${baseImage}w154${source}`} />
     <Text>{text}</Text>
   </Wrapper>

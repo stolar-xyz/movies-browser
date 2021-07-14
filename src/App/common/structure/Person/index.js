40cd09bae0267706a132493e5d1150d5fc447e62
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-14 14:14:48 +0200

    deleted Overlay component and added ProfileIcon to src depending on source

--- src/App/common/structure/Person/index.js
+++ src/App/common/structure/Person/index.js
@@ -1,14 +1,13 @@
-import Overlay from '../Overlay';
 import { StyledPerson, Image, Wrapper, Name, Role } from './styled';
 import { baseImage } from '../../../apiDetails';
+import ProfileIcon from '../../../assets/svgs/profile.svg';
 
 const Person = ({ name, role, source }) => (
   <StyledPerson>
-    {source ? (
-      <Image alt={`photo of ${name}`} src={`${baseImage}w342${source}`} />
-    ) : (
-      <Overlay person />
-    )}
+    <Image
+      alt={`photo of ${name}`}
+      src={source ? `${baseImage}w342${source}` : ProfileIcon}
+    />
     <Wrapper>
       <Name>{name}</Name>
       {role && <Role>{role}</Role>}

ce583de08b54d7644cf23f2eeb9f049c4cc7ea48
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-14 14:16:06 +0200

    added ProfileIcon and ProfileIcon to src depending on source and pathname

--- src/App/features/Search/ResultsBox/ResultTile/index.js
+++ src/App/features/Search/ResultsBox/ResultTile/index.js
@@ -1,10 +1,21 @@
 import { baseImage } from '../../../../apiDetails';
 import { toMovie, toPerson } from '../../../../routes';
 import { Image, Text, Wrapper } from './styled';
+import VideoIcon from '../../../../assets/svgs/videoExtra.svg';
+import ProfileIcon from '../../../../assets/svgs/profile.svg';
 
 const ResultTile = ({ pathname, id, source, text }) => (
   <Wrapper to={pathname === 'movie' ? toMovie(id) : toPerson(id)}>
-    <Image alt={text} src={`${baseImage}w154${source}`} />
+    <Image
+      alt={text}
+      src={
+        source
+          ? `${baseImage}w154${source}`
+          : pathname === 'movie'
+          ? VideoIcon
+          : ProfileIcon
+      }
+    />
     <Text>{text}</Text>
   </Wrapper>
 );

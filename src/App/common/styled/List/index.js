e10f376c64575d0f0397cbefff0d4ea227b555a0
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-15 21:46:04 +0200

    increased template columns on people prop

--- src/App/common/styled/List/index.js
+++ src/App/common/styled/List/index.js
@@ -25,7 +25,7 @@ const List = styled.ul`
   ${({ people }) =>
     people &&
     css`
-      grid-template-columns: repeat(auto-fill, 200px);
+      grid-template-columns: repeat(auto-fill, 202px);
 
       @media (max-width: ${({ theme }) => theme.mobileMax}px) {
         grid-template-columns: repeat(auto-fill, 136px);

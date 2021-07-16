05f46c91da97869577ffaf0eb87c61904661a296
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-16 18:33:50 +0200

    added StyledLink to structure with the right props

--- src/App/common/structure/Movie/index.js
+++ src/App/common/structure/Movie/index.js
@@ -1,26 +1,30 @@
 import { StyledMovie, Title, Year, Wrapper, Container } from './styled';
+import { StyledLink } from '../../styled/Link';
+import { toMovie } from '../../../routes';
 import Genres from '../../../features/movies/Genres';
 import Rating from '../Rating';
 import VideoIcon from '../../../assets/svgs/videoExtra.svg';
 import Image from '../Image';
 
-const Movie = ({ title, year, genres, rate, votes, source }) => (
+const Movie = ({ title, year, genres, rate, votes, source, id }) => (
   <StyledMovie>
-    <Image
-      movie
-      altText={`${title} movie poster`}
-      source={source}
-      icon={VideoIcon}
-      size={'w342'}
-    />
-    <Wrapper>
-      <Container>
-        <Title>{title}</Title>
-        {year && <Year>{year.slice(0, 4)}</Year>}
-        <Genres genres={genres} />
-      </Container>
-      <Rating small rate={rate} votes={votes} />
-    </Wrapper>
+    <StyledLink movie={'true'} to={toMovie(id)}>
+      <Image
+        movie
+        altText={`${title} movie poster`}
+        source={source}
+        icon={VideoIcon}
+        size={'w342'}
+      />
+      <Wrapper>
+        <Container>
+          <Title>{title}</Title>
+          {year && <Year>{year.slice(0, 4)}</Year>}
+          <Genres genres={genres} />
+        </Container>
+        <Rating small rate={rate} votes={votes} />
+      </Wrapper>
+    </StyledLink>
   </StyledMovie>
 );
 
--- src/App/common/structure/Person/index.js
+++ src/App/common/structure/Person/index.js
@@ -1,20 +1,24 @@
 import { StyledPerson, Wrapper, Name, Role } from './styled';
+import { StyledLink } from '../../styled/Link';
+import { toPerson } from '../../../routes';
 import ProfileIcon from '../../../assets/svgs/profile.svg';
 import Image from '../Image';
 
-const Person = ({ name, role, source }) => (
+const Person = ({ name, role, source, id }) => (
   <StyledPerson>
-    <Image
-      person
-      altText={`photo of ${name}`}
-      source={source}
-      icon={ProfileIcon}
-      size={'w342'}
-    />
-    <Wrapper>
-      <Name>{name}</Name>
-      {role && <Role>{role}</Role>}
-    </Wrapper>
+    <StyledLink to={toPerson(id)}>
+      <Image
+        person
+        altText={`photo of ${name}`}
+        source={source}
+        icon={ProfileIcon}
+        size={'w342'}
+      />
+      <Wrapper>
+        <Name>{name}</Name>
+        {role && <Role>{role}</Role>}
+      </Wrapper>
+    </StyledLink>
   </StyledPerson>
 );
 

c1b89be7e415cba24c8c8b789a7b4ad1a45abf25
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-16 18:31:47 +0200

    added Link styled component with movie prop

---
+++ src/App/common/styled/Link/index.js
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+  cursor: pointer;
+  color: ${({ theme }) => theme.color.text.primaryText};
+  transition: box-shadow 0.3s ease-out;
+
+  &:hover {
+    box-shadow: 0 6px 0 0 #fcd420;
+
+    @media (max-width: ${({ theme }) => theme.mobileMax}px) {
+      box-shadow: ${({ movie }) =>
+        movie ? '4px 0 0 0 #fcd420;' : '0 4px 0 0 #fcd420;'};
+    }
+  }
+
+  ${({ movie }) =>
+    movie &&
+    css`
+      min-height: 675px;
+
+      @media (max-width: ${({ theme }) => theme.mobileMax}px) {
+        flex-grow: 1;
+        flex-direction: row;
+        min-height: unset;
+      }
+    `}
+`;

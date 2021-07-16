367d0b9409768e8e6b2ddc7068b820f78f0c4782
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-16 18:36:32 +0200

    deleted unused styles in styled components

--- src/App/common/structure/Movie/styled.js
+++ src/App/common/structure/Movie/styled.js
@@ -7,29 +7,11 @@ export const Wrapper = styled.div`
   flex-grow: 1;
   justify-content: space-between;
   padding: 16px;
-  transition: box-shadow 0.3s ease-out;
 `;
 
 export const StyledMovie = styled.li`
-  color: ${({ theme }) => theme.color.text.primaryText};
   display: flex;
-  flex-direction: column;
   box-shadow: 0px 4px 12px ${({ theme }) => theme.color.element.boxShadow};
-  min-height: 650px;
-  cursor: pointer;
-
-  &:hover ${Wrapper} {
-    box-shadow: 0 6px 0 0 #fcd420;
-
-    @media (max-width: ${({ theme }) => theme.mobileMax}px) {
-      box-shadow: 4px 0 0 0 #fcd420;
-    }
-  }
-
-  @media (max-width: ${({ theme }) => theme.mobileMax}px) {
-    flex-direction: row;
-    min-height: unset;
-  }
 `;
 
 export const Container = styled.div`
--- src/App/common/structure/Person/styled.js
+++ src/App/common/structure/Person/styled.js
@@ -7,7 +7,6 @@ export const Wrapper = styled.div`
   overflow-wrap: anywhere;
   flex-grow: 1;
   text-align: center;
-  transition: box-shadow 0.3s ease-out;
   background: ${({ theme }) => theme.color.background.secondaryBackground};
 
   @media (max-width: ${({ theme }) => theme.mobileMax}px) {
@@ -16,20 +15,8 @@ export const Wrapper = styled.div`
 `;
 
 export const StyledPerson = styled.li`
-  color: ${({ theme }) => theme.color.text.primaryText};
   display: flex;
-  flex-direction: column;
   box-shadow: 0px 4px 12px ${({ theme }) => theme.color.element.boxShadow};
-  border-radius: 5px 5px 0 0;
-  cursor: pointer;
-
-  &:hover ${Wrapper} {
-    box-shadow: 0 6px 0 0 #fcd420;
-
-    @media (max-width: ${({ theme }) => theme.mobileMax}px) {
-      box-shadow: 0 4px 0 0 #fcd420;
-    }
-  }
 `;
 
 export const Name = styled.span`

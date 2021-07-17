ddbeff72fd9bc4d31551a3d0f5b4c753ea6720aa
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-17 13:14:08 +0200

    added itemSlice file

---
+++ src/App/features/itemSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const itemSlice = createSlice({
+  name: 'item',
+  initialState: {
+    result: [],
+    status: 'initial',
+  },
+  reducers: {
+    fetchItem: state => {
+      state.status = 'loading';
+    },
+    fetchItemSuccess: (state, { payload }) => {
+      state.result = payload;
+      state.status = 'success';
+    },
+    fetchItemError: state => {
+      state.status = 'error';
+    },
+  },
+});
+
+export const {
+  fetchItem,
+  fetchItemSuccess,
+  fetchItemError,
+  setActivePath,
+  resetState,
+} = itemSlice.actions;
+
+export const selectResult = state => state.item.result;
+export const selectStatus = state => state.item.status;
+
+export default itemSlice.reducer;

1ce0d3de56c957a29110a714111c629c4e71e2f1
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-17 13:55:08 +0200

    added tile prop to structure

--- src/App/common/structure/Image/index.js
+++ src/App/common/structure/Image/index.js
@@ -1,11 +1,12 @@
 import { StyledImage } from './styled';
 import { baseImage } from '../../../apiDetails';
 
-const Image = ({ altText, source, icon, size, movie, person, search }) => (
+const Image = ({ altText, source, icon, size, movie, person, search, tile }) => (
   <StyledImage
     movie={movie}
     person={person}
     search={search}
+    tile={tile}
     alt={altText}
     src={source ? `${baseImage}${size}${source}` : icon}
   />

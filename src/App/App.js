817a1a42c36bfa4be24b47d6e06825bda8601236
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-17 13:16:37 +0200

    added new routes to subpages

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,16 @@
-import Navigation from './common/structure/Navigation';
-import MoviesPage from './features/movies/MoviesPage';
-import PeoplePage from './features/people/PeoplePage';
 import { Normalize } from 'styled-normalize';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from './theme';
 import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
-import { toMovies, toPeople } from './routes';
+import { toMovie, toMovies, toPeople, toPerson } from './routes';
 import { useSelector } from 'react-redux';
 import { selectTheme } from './features/ThemeToggler/themeSlice';
+import Navigation from './common/structure/Navigation';
+import MoviesPage from './features/movies/MoviesPage';
+import PeoplePage from './features/people/PeoplePage';
+import PersonPage from './features/people/PersonPage';
+import MoviePage from './features/movies/MoviePage';
 
 const App = () => {
   const isTheme = useSelector(selectTheme);
@@ -21,8 +23,14 @@ const App = () => {
         <Navigation />
         <main>
           <Switch>
+            <Route path={toMovie(':id')}>
+              <MoviePage />
+            </Route>
             <Route path={toMovies()}>
               <MoviesPage />
+            </Route>
+            <Route path={toPerson(':id')}>
+              <PersonPage />
             </Route>
             <Route path={toPeople()}>
               <PeoplePage />

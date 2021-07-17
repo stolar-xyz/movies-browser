cd6775deebf4223395725ea128796f46f01c27d6
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-17 13:14:58 +0200

    passed itemSaga to rootSaga

--- src/App/rootSaga.js
+++ src/App/rootSaga.js
@@ -3,7 +3,8 @@ import { genresSaga } from './features/movies/Genres/genresSaga';
 import { listSaga } from './features/listSaga';
 import { themeSaga } from './features/ThemeToggler/themeSaga';
 import { searchSaga } from './features/Search/searchSaga';
+import { itemSaga } from './features/itemSaga';
 
 export default function* rootSaga() {
-  yield all([themeSaga(), listSaga(), genresSaga(), searchSaga()]);
+  yield all([themeSaga(), listSaga(), genresSaga(), searchSaga(), itemSaga()]);
 }

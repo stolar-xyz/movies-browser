2799e5acb111e844ab2defc083ddaf1559a716ef
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-18 12:22:28 +0200

    changed margin

--- src/App/common/styled/Section/index.js
+++ src/App/common/styled/Section/index.js
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 const Section = styled.section`
   max-width: 1368px;
-  margin: 56px auto 0;
+  margin: 56px auto;
   padding: 0 16px;
 
   @media (max-width: ${({ theme }) => theme.mobileMax}px) {
-    margin: 24px auto 0;
+    margin: 24px auto;
   }
 `;
 

98b3b7aead83f5ba994f434c4246a4f268b6549f
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-20 18:08:53 +0200

    deleted Wrapper and added ListItem and StyledLink to structure

--- src/App/features/Search/ResultsBox/ResultTile/index.js
+++ src/App/features/Search/ResultsBox/ResultTile/index.js
@@ -1,5 +1,5 @@
 import { toMovie, toPerson } from '../../../../routes';
-import { Text, Wrapper } from './styled';
+import { ListItem, StyledLink, Text } from './styled';
 import VideoIcon from '../../../../assets/svgs/videoExtra.svg';
 import ProfileIcon from '../../../../assets/svgs/profile.svg';
 import Image from '../../../../common/structure/Image';
@@ -8,16 +8,18 @@ const ResultTile = ({ pathname, id, source, text }) => {
   const isPathnameEqual = pathname === 'movie';
 
   return (
-    <Wrapper to={isPathnameEqual ? toMovie(id) : toPerson(id)}>
-      <Image
-        search
-        altText={isPathnameEqual ? `${text} movie poster` : `photo of ${text}`}
-        source={source}
-        icon={isPathnameEqual ? VideoIcon : ProfileIcon}
-        size={'w154'}
-      />
-      <Text>{text}</Text>
-    </Wrapper>
+    <ListItem>
+      <StyledLink to={isPathnameEqual ? toMovie(id) : toPerson(id)}>
+        <Image
+          search
+          altText={isPathnameEqual ? `${text} movie poster` : `photo of ${text}`}
+          source={source}
+          icon={isPathnameEqual ? VideoIcon : ProfileIcon}
+          size={'w154'}
+        />
+        <Text>{text}</Text>
+      </StyledLink>
+    </ListItem>
   );
 };
 

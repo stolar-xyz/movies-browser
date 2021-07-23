20ab9e8f36786a5af6124d806d10d3b94a288720
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-23 18:04:25 +0200

    changed styles

--- src/App/common/structure/Person/styled.js
+++ src/App/common/structure/Person/styled.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
+export const StyledPerson = styled.li`
+  display: flex;
+  box-shadow: 0px 4px 12px ${({ theme }) => theme.color.element.boxShadow};
+`;
+
 export const Wrapper = styled.div`
   display: grid;
-  place-items: center;
-  padding: 16px;
+  padding: 14px;
   overflow-wrap: anywhere;
   flex-grow: 1;
   text-align: center;
@@ -14,14 +18,10 @@ export const Wrapper = styled.div`
   }
 `;
 
-export const StyledPerson = styled.li`
-  display: flex;
-  box-shadow: 0px 4px 12px ${({ theme }) => theme.color.element.boxShadow};
-`;
-
 export const Name = styled.span`
   font-size: 18px;
   font-weight: 500;
+  align-self: center;
 
   @media (max-width: ${({ theme }) => theme.mobileMax}px) {
     font-size: 14px;
@@ -29,13 +29,10 @@ export const Name = styled.span`
 `;
 
 export const Role = styled.span`
-  display: block;
-  margin-top: 8px;
-  font-size: 16px;
+  margin-top: 0.2em;
   color: ${({ theme }) => theme.color.text.secondaryText};
 
   @media (max-width: ${({ theme }) => theme.mobileMax}px) {
-    font-size: 12px;
-    margin-top: 4px;
+    font-size: 10px;
   }
 `;

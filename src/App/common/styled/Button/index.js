80465c975a9afe92e4a132430362778d209da30b
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-23 17:40:35 +0200

    added Button styled component

---
+++ src/App/common/styled/Button/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  font-weight: 600;
+  font-size: 12px;
+  padding: 6px 12px;
+  border-radius: 5px;
+  border: none;
+  background: #fcd420;
+  color: #18181b;
+  box-shadow: 0px 4px 12px ${({ theme }) => theme.color.element.boxShadow};
+  cursor: pointer;
+  transition: transform 0.3s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  &:active {
+    box-shadow: inset 0px 2px 0px rgba(20, 70, 32, 0.2);
+  }
+`;
+
+export default Button;

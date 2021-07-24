6a58bcfdfdac06833347bcf4bfc56110ceba1f55
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-24 15:51:14 +0200

    added displayedCast and displayedCrew with reducers and selects

--- src/App/features/itemSlice.js
+++ src/App/features/itemSlice.js
@@ -5,6 +5,8 @@ const itemSlice = createSlice({
   initialState: {
     result: [],
     resultDetails: [],
+    displayedCast: null,
+    displayedCrew: null,
     status: 'initial',
   },
   reducers: {
@@ -13,22 +15,45 @@ const itemSlice = createSlice({
     },
     fetchItemSuccess: (
       state,
-      { payload: { responseData, responseDetailsData } }
+      {
+        payload: {
+          responseData,
+          responseDetailsData,
+          castItemsLength,
+          crewItemsLength,
+        },
+      }
     ) => {
       state.result = responseData;
       state.resultDetails = responseDetailsData;
+      state.displayedCast = castItemsLength;
+      state.displayedCrew = crewItemsLength;
       state.status = 'success';
     },
     fetchItemError: state => {
       state.status = 'error';
     },
+    setCastLength: (state, { payload }) => {
+      state.displayedCast = payload;
+    },
+    setCrewLength: (state, { payload }) => {
+      state.displayedCrew = payload;
+    },
   },
 });
 
-export const { fetchItem, fetchItemSuccess, fetchItemError } = itemSlice.actions;
+export const {
+  fetchItem,
+  fetchItemSuccess,
+  fetchItemError,
+  setCastLength,
+  setCrewLength,
+} = itemSlice.actions;
 
 export const selectResult = state => state.item.result;
 export const selectResultDetails = state => state.item.resultDetails;
+export const selectDisplayedCast = state => state.item.displayedCast;
+export const selectDisplayedCrew = state => state.item.displayedCrew;
 export const selectStatus = state => state.item.status;
 
 export default itemSlice.reducer;
